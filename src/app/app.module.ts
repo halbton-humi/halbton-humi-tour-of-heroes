e95fa897d76eae97233e79612297df676fd5109a
halbton-humi <tmueller@halbton.net>
2020-05-25 17:02:19 +0200

    4 files modified, 9 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { AppComponent } from "./app.component";
+import { HeroesComponent } from "./heroes/heroes.component";
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
+import { HeroService } from "./hero.service";
+import { MessagesComponent } from "./messages/messages.component";
+import { MessageService } from "./message.service";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   declarations: [
     AppComponent,
-    HelloComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent
   ],
-  providers: [
-
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+  providers: [HeroService, MessageService],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
